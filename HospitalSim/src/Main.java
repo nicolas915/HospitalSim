ac112f66e019c2a409e5a5ff87ac192d6ddcea7b
NeyKoZv <51635179+NeyKoZv@users.noreply.github.com>
2022-12-11 17:54:04 -0500

    Besoin de l'observer pour continuer

--- HospitalSim/src/Main.java
+++ HospitalSim/src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import Organ.Organ;
+import Organ.OrganDispatcher;
 import Organ.OrganGenerator;
 import People.Patient;
 import People.PatientGenerator;
@@ -14,6 +15,8 @@ public class Main {
         
         PatientGenerator pg = new PatientGenerator();
         OrganGenerator og = new OrganGenerator();
+        
+        OrganDispatcher od = new OrganDispatcher();
         
         ArrayList<Patient> listPatient = new ArrayList<>();
         ArrayList<Organ> listOrgan = new ArrayList<>();
@@ -33,6 +36,8 @@ public class Main {
         			buffer.run();
         		}
         	}
+        	
+        	
         	
         	for(int i = 0; i < listOrgan.size(); i++) {
         		if(listOrgan.get(i).getState() == Organ.ROTTEN) {
--- HospitalSim/src/Organ/OrganDispatcher.java
+++ HospitalSim/src/Organ/OrganDispatcher.java
@@ -1,4 +1,21 @@
 package Organ;
 
+import java.util.ArrayList;
+
+import People.Patient;
+
 public class OrganDispatcher {
+    
+    public OrganDispatcher() {
+	}
+    
+    public int DispatchOrgan(Organ organ, ArrayList<Patient> listOfPatient) {
+    	for(int i = 0; i < listOfPatient.size(); i++) {
+    		Patient buffer = listOfPatient.get(i);
+    		if(buffer.getWaitedOrgan().equals(organ.getName())) {
+    			return i;
+    		}
+    	}
+		return -1;
+    }
 }
